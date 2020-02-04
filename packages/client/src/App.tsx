e276fb3bfd327624e6c1370293b481a5948f1703
Daniel <danielbuterus@gmail.com>
2020-02-05 01:56:57 +0600

    the repository has been moved

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import { Toolbar, IconButton, Typography, AppBar, CssBaseline, Drawer, Divider, List } from '@material-ui/core';
+import {
+    Menu as MenuIcon,
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon,
+} from '@material-ui/icons/';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-github';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            height: '100vh',
+        },
+        appBar: {
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        hide: {
+            display: 'none',
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 1),
+            ...theme.mixins.toolbar,
+            justifyContent: 'space-between',
+        },
+        content: {
+            flexGrow: 1,
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: -drawerWidth,
+        },
+        contentShift: {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        },
+    }),
+);
+
+const App: React.FC = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    const onChange = (newValue: any) => {
+        console.log('change', newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h5" noWrap>
+                        Form Editor
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <Typography variant="h5" noWrap>
+                        Commands
+                    </Typography>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List></List>
+                <Divider />
+                <List></List>
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+                style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}
+            >
+                <div className={classes.drawerHeader} />
+                <div style={{ width: '100%', height: '64', display: 'flex', alignItems: 'center', flex: '1 0 auto' }}>
+                    <Typography variant="h6" noWrap style={{ width: '50%', paddingLeft: '40px' }}>
+                        Command:
+                    </Typography>
+                    <Typography variant="h6" noWrap style={{ width: '50%', paddingLeft: '40px' }}>
+                        Form:
+                    </Typography>
+                </div>
+                <div style={{ width: '100%', height: `calc(100vh - 128px)`, display: 'flex', flex: '0 0 auto' }}>
+                    <AceEditor
+                        mode="json"
+                        theme="github"
+                        onChange={onChange}
+                        name="UNIQUE_ID_OF_DIV"
+                        editorProps={{ $blockScrolling: true }}
+                        style={{ width: '50%', height: '100%' }}
+                    />
+                    <AceEditor
+                        mode="json"
+                        theme="github"
+                        onChange={onChange}
+                        name="UNIQUE_ID_OF_DIV"
+                        editorProps={{ $blockScrolling: true }}
+                        style={{ width: '50%', height: '100%' }}
+                        readOnly={true}
+                    />
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default App;
